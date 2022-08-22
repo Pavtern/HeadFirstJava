b2f3a5fde476b78dc1cf69695b6bb780b4c078ea
Pavtern <ternavskii.pavel@gmail.com>
2022-08-22 20:04:02 +0200

    game the final version

---
+++ src/main/java/DotComGame/DotComTestDrive.java
@@ -0,0 +1,13 @@
+//package DotComGame;
+//
+//import java.util.ArrayList;
+//
+//public class DotComTestDrive {
+//    public static void main(String[] args) {
+//        DotComBust game = new DotComBust();
+//        game.setUpGame();
+//        for (DotCom dotComSet : game.dotComsList) {
+//            dotComSet.();
+//        }
+//    }
+//}
--- src/main/java/MultiFor.java
+++ src/main/java/MultiFor.java
@@ -3,10 +3,6 @@ public class MultiFor {
         for (int x = 0; x < 4; x++) {
             for (int y = 4; y > 2; y--) {
                 System.out.println(x + " " + y);
-
-                if (x == y){
-                    break;
-                }
             }
             if (x == 1) {
                 x++;
